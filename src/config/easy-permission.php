e84a3cff374ad9c28410a14c20c29b2fd6c62af3
chenggx <chenggx@users.noreply.github.com>
2020-08-15 00:11:54 +0000

    Apply fixes from StyleCI

--- src/config/easy-permission.php
+++ src/config/easy-permission.php
@@ -5,54 +5,54 @@ return [
     'permission_cache_suffix' => '_permissions',
 
     //按钮权限默认缓存时间（单位：天）
-    'button_cache_time'       => 1,
+    'button_cache_time' => 1,
 
     // 用户表关联角色表的字段名称
-    'role_field'              => 'role_id',
+    'role_field' => 'role_id',
 
-    'router'          => [
+    'router' => [
         //不需要验证权限的路由列表
         'white_list' => [
             //'auth.login'   //示例：路由名称
         ],
         'middleware' => ['api', 'auth', 'check-permission'],
         //路由地址前缀
-        'prefix'     => 'api',
+        'prefix' => 'api',
     ],
     // 权限类型
     'permission_type' => [
-        'group'  => 1,
-        'page'   => 2,
+        'group' => 1,
+        'page' => 2,
         'button' => 3,
     ],
 
     'table_names' => [
-        'roles'                    => 'roles',
-        'permissions'              => 'permissions',
+        'roles' => 'roles',
+        'permissions' => 'permissions',
         'role_permission_relation' => 'role_permission_relation',
     ],
 
     'table_column_names' => [
-        'roles_table'                    => [
-            'id'     => 'id',
-            'name'   => 'name',
+        'roles_table' => [
+            'id' => 'id',
+            'name' => 'name',
             'remark' => 'remark',
         ],
-        'permission_table'               => [
-            'id'     => 'id',
-            'pid'    => 'pid',
-            'title'  => 'title',
-            'name'   => 'name',
-            'type'   => 'type',
-            'order'  => 'order',
-            'icon'   => 'icon',
+        'permission_table' => [
+            'id' => 'id',
+            'pid' => 'pid',
+            'title' => 'title',
+            'name' => 'name',
+            'type' => 'type',
+            'order' => 'order',
+            'icon' => 'icon',
             'method' => 'method',
-            'api'    => 'api',
+            'api' => 'api',
             'hidden' => 'hidden',
         ],
         'role_permission_relation_table' => [
-            'id'            => 'id',
-            'role_id'       => 'role_id',
+            'id' => 'id',
+            'role_id' => 'role_id',
             'permission_id' => 'permission_id',
         ],
     ],
